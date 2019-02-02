daa58bfe6821f376238df3797055326b09df6d1b
Jdraiv <josuedr11@gmail.com>
2019-02-02 17:06:35 -0600

    Modified sorting tester method

--- sorting/tester.rb
+++ sorting/tester.rb
@@ -5,10 +5,13 @@ require 'test/unit/assertions'
 
 def test_algo(arr, algo, failure_message="Wrong answer")
     # This method use the sort built in function of Ruby to test sorting algorithms
+    arr_copy = arr.dup
+    result = algo.call(arr_copy)
+    expected = arr.sort!
 
-    if arr.sort == algo.call(arr)
-        puts "Success"
+    if expected == result
+        puts "Success. Result [%s]" % result.join(',')
     else
-        puts failure_message
+        puts "%s. Result: [%s], Expected Result: [%s]" % [failure_message, result.join(','), expected.join(',')]
     end
 end
